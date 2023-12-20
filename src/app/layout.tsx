290887497832b13a0d81c31058452945e99f2463
Michael Matheus <michaelmatheus2001@gmail.com>
2023-12-20 16:23:50 -0300

    feat: add default header & footer on main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
 import type { Metadata } from 'next'
 import { JetBrains_Mono, Inter } from 'next/font/google'
+
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+
 import './globals.css'
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ['latin'],
-  display: 'swap'
+  display: 'swap',
 })
 const inter = Inter({
   subsets: ['latin'],
-  display: 'swap'
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
   title: 'Portfólio Desenvolvedor Full-Stack - Michael Matheus',
-  description: 'Sou Michael Matheus, desenvolvedor Full-stack JavaScript. Descubra meu mundo de códigos inovadores. Convido você a explorar meus projetos ou me contratar para desafios incríveis.',
+  description:
+    'Sou Michael Matheus, desenvolvedor Full-stack JavaScript. Descubra meu mundo de códigos inovadores. Convido você a explorar meus projetos ou me contratar para desafios incríveis.',
 }
 
 export default function RootLayout({
@@ -23,7 +28,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={jetBrainsMono.className}>{children}</body>
+      <body className={`${jetBrainsMono.className} ${'overflow-x-hidden'}`}>
+        <div className="flex flex-col h-screen w-11/12 lg:w-[1120px] m-auto">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </body>
     </html>
   )
 }
