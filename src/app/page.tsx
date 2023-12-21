ddd616d9c6e89134fc3042dd45537e2b980ecfff
Michael Matheus <michaelmatheus2001@gmail.com>
2023-12-21 15:56:59 -0300

    feat: add Project component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import { Intro } from './components/Intro'
 import { Skills } from './components/Skills'
+import { Projects } from './components/Projects'
 
 export default function Home() {
   return (
     <main>
       <Intro />
       <Skills />
-
-      <section className="text-gray-200 py-2 px-10 mt-40">
-        Projetos Pessoais
-      </section>
+      <Projects />
+      
       <section className="text-gray-200 py-2 px-10">Sobre Mim</section>
       <section className="text-gray-200 py-2 px-10">CTA</section>
     </main>
