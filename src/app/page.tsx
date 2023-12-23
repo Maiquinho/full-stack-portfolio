4f3d56ed1f2dff5d47e11a5022804346eba5cd97
Michael Matheus <michaelmatheus2001@gmail.com>
2023-12-23 10:16:12 -0300

    feat: add About component on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Intro } from './components/Intro'
 import { Skills } from './components/Skills'
-import { Projects } from './components/Projects'
+import { Projects } from './components/Projects/'
+import { About } from './components/About'
 
 export default function Home() {
   return (
@@ -8,8 +9,8 @@ export default function Home() {
       <Intro />
       <Skills />
       <Projects />
+      <About />
 
-      <section className="text-gray-200 py-2 px-10">Sobre Mim</section>
       <section className="text-gray-200 py-2 px-10">CTA</section>
     </main>
   )
