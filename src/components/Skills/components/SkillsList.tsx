35842e69a07870636419bb75c768b1348b973b6c
Michael Matheus <michaelmatheus2001@gmail.com>
2023-12-26 14:51:27 -0300

    feat: add skills data from Hygraph

--- src/components/Skills/components/SkillsList.tsx
+++ src/components/Skills/components/SkillsList.tsx
@@ -1,63 +1,37 @@
 'use client'
 
-import Flicking from '@egjs/react-flicking'
 import Image from 'next/image'
+
+import { gql, useQuery } from '@apollo/client'
+
+import Flicking from '@egjs/react-flicking'
 
 import '@egjs/react-flicking/dist/flicking.css'
 
 import './styles.css'
 
 interface Skill {
-  id: number
-  image: string
+  id: string
   title: string
+  image: {
+    url: string
+  }
 }
 
-export function SkillsList() {
-  const skillsData: Skill[] = [
-    {
-      id: 1,
-      image: '/assets/skills/javascript-icon.png',
-      title: 'JavaScript',
-    },
-    {
-      id: 2,
-      image: '/assets/skills/reactjs-icon.png',
-      title: 'React.js',
-    },
-    {
-      id: 3,
-      image: '/assets/skills/nextjs-icon.png',
-      title: 'Next.js',
-    },
-    {
-      id: 4,
-      image: '/assets/skills/nodejs-icon.png',
-      title: 'Node.js',
-    },
-    {
-      id: 5,
-      image: '/assets/skills/typescript-icon.png',
-      title: 'TypeScript',
-    },
-    {
-      id: 6,
-      image: '/assets/skills/mariadb-logo-white.png',
-      title: 'MariaDB',
-    },
-    {
-      id: 7,
-      image: '/assets/skills/styled-components.png',
-      title: 'Styled Components',
-    },
-    {
-      id: 8,
-      image: '/assets/skills/tailwindcss.png',
-      title: 'TailwindCSS',
-    },
-  ]
+const GET_SKILLS_QUERY = gql`
+  query GetSkills {
+    skills {
+      id
+      title
+      image {
+        url
+      }
+    }
+  }
+`
 
-  //   const [skills, setSkills] = useState<Skill[]>([])
+export function SkillsList() {
+  const { data } = useQuery<{ skills: Skill[] }>(GET_SKILLS_QUERY)
 
   return (
     <Flicking
@@ -66,12 +40,18 @@ export function SkillsList() {
       align="prev"
       autoResize={true}
     >
-      {skillsData.map((skill) => (
+      {data?.skills.map((skill) => (
         <div
           key={skill.id}
           className="flex flex-col gap-4 items-center justify-between w-min panel hover:opacity-100"
         >
-          <Image src={skill.image} width={87} height={87} alt="" />
+          <Image
+            src={skill.image.url}
+            width={87}
+            height={87}
+            alt={`ícone descritivo da tecnologia ${skill.title}`}
+            title={skill.title}
+          />
           <span className="text-gray-100 text-md">{skill.title}</span>
         </div>
       ))}
